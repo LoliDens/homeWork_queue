4b09c0775e13acf6b9cbc6a0a5b23fa937fc7347
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-14 20:29:44 +0500

    Добавьте файлы проекта.

---
+++ homeWork_queue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork_queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int queueWaitingTime = 10;
+            int coutGranny;
+
+            Console.Write("Введи количесто старушек в очерди: ");
+            coutGranny = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Вы должны простоять:{coutGranny * queueWaitingTime / 60 } и {coutGranny * queueWaitingTime % 60} минут");
+            Console.ReadKey();
+        }
+    }
+}
